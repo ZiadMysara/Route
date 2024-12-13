cf683df579c430ee49178429bdcec2c75ea26b70
ZiadMysara <ziadmysara@gmail.com>
2024-12-14 00:06:51 +0200

    done

---
+++ Assignment3.cs
@@ -0,0 +1,446 @@
+﻿namespace Session_03;
+
+class Assignment3
+{
+    static void Main(string[] args)
+    {
+         DemoNotes.Run();
+         // P1.Run();   // 1
+         // P2.Run();   // 2
+         // P3.Run();   // 3
+         // P4.Run();   // 4
+         // P5.Run();   // 5
+         // P6.Run();   // 7
+         // P7.Run();   // 9
+         // P8.Run();   // 10
+         // P9.Run();   // 11
+         // P10.Run();  // 17
+         // P11.Run();  // 18
+         // P12.Run();  // 21
+         // P13.Run();  // 22
+         // P14.Run();  // 23
+         // P15.Run();  // 24
+         // P16.Run();  // 25
+    }
+}
+
+
+
+internal static class P1
+{
+    public static void Run()
+    {
+        // Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no.
+        // Example (1)
+        // Input: 12 
+        // Output: Yes
+        // Example (2)
+        // Input: 9 
+        // Output: No
+
+        Console.WriteLine("Enter a number:");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number % 3 == 0 && number % 4 == 0)
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        {
+            Console.WriteLine("No");
+        }
+    }
+}
+
+internal static class P2
+{
+    public static void Run()
+    {
+        // Write a program that allows the user to insert an integer then print negative if it is negative number otherwise print positive.
+        // Example (1)
+        // Input: -5
+        // Output: negative
+        // Example (2)
+        // Input: 10
+        // Output: positive
+
+        Console.WriteLine("Enter a number:");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number < 0)
+        {
+            Console.WriteLine("negative");
+        }
+        else
+        {
+            Console.WriteLine("positive");
+        }
+    }
+}
+
+internal static class P3
+{
+    public static void Run()
+    {
+        // Write a program that takes 3 integers from the user then prints the max element and the min element.
+        // Example (1)
+        // Input:7,8,5
+        // Output:
+        // max element = 8
+        // min element = 5
+        // Example (2)
+        // Input: 3 6 9
+        // Outputs:
+        // Max element = 9
+        // Min element = 3
+
+        Console.WriteLine("Enter 3 numbers:");
+        string[] numbers = Console.ReadLine().Split(' ');
+
+        int max = int.Parse(numbers[0]);
+        int min = int.Parse(numbers[0]);
+
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            int number = int.Parse(numbers[i]);
+
+            if (number > max)
+            {
+                max = number;
+            }
+
+            if (number < min)
+            {
+                min = number;
+            }
+        }
+
+        Console.WriteLine($"Max element = {max}");
+        Console.WriteLine($"Min element = {min}");
+    }
+}
+
+internal static class P4
+{
+    public static void Run()
+    {
+        // Write a program that allows the user to insert an integer number then check
+        // If a number is even or odd. 
+        int num = int.Parse(Console.ReadLine());
+        if (num % 2 == 0)
+        {
+            Console.WriteLine("Num is even");
+        }
+        else
+        {
+            Console.WriteLine("Num is odd");
+        }
+
+
+    }
+}
+
+internal static class P5
+{
+    public static void Run()
+    {
+        // Write a program that takes character from the user then if it is
+        // a vowel chars (a,e,I,o,u) then print (vowel) otherwise print (consonant).
+        // Example (1)
+        // Input: O
+        // Output: vowel
+        // Example (2)
+        // Input: b
+        // Output: Consonant
+        // 'A' = 65
+        // 'a' = 97
+        // 'Z'
+        Console.WriteLine("Enter a character:");
+        char c = char.Parse(Console.ReadLine());
+        if (c >= 'a')
+        {
+            c -= (char)32;
+        }
+        if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+        {
+            Console.WriteLine("vowel");
+        }
+        else
+        {
+            Console.WriteLine("Consonant");
+        }
+
+    }
+}
+
+internal static class P6
+{
+    public static void Run()
+    {
+        // Write a program that allows the user to insert an integer then 
+        // print a multiplication table up to 12.
+        // Example
+        // Input: 5
+        // Outputs: 5 10 15 20 25 30 35 40 45 50 55 60
+        int num = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= 12; i++)
+        {
+            Console.Write($"{num * i} ");
+        }
+
+    }
+}
+
+internal static class P7
+{
+    public static void Run()
+    {
+        // Write a program that takes two integers then prints the power.
+        // Example:
+        // Input: 4 3
+        // Output: 64
+        // Hint: how to calculate 4^3 = 4 * 4 * 4 =64
+        string[] numbers = Console.ReadLine().Split(' ');
+        int num1 = int.Parse(numbers[0]);
+        int num2 = int.Parse(numbers[1]);
+        int result = 1;
+        for (int i = 0; i < num2; i++)
+        {
+            result *= num1;
+        }
+        Console.WriteLine(result);
+
+    }
+}
+
+internal static class P8
+{
+    public static void Run()
+    {
+        // Write a program to enter marks of five subjects and calculate total,
+        // average and percentage.
+        // Example
+        // Input: - Enter Marks of five subjects: 95 76 58 90 89
+        // Output: Total marks = 408
+        // Average Marks = 81
+        // Percentage = 81
+        Console.WriteLine("Enter Marks of five subjects: ");
+        string[] marks = Console.ReadLine().Split(' ');
+        int total = 0;
+        for (int i = 0; i < marks.Length; i++)
+        {
+            total += int.Parse(marks[i]);
+        }
+        Console.WriteLine($"Total marks = {total}");
+        Console.WriteLine($"Average Marks = {total / marks.Length}");
+        Console.WriteLine($"Percentage = {total / marks.Length}");
+
+    }
+}
+
+internal static class P9
+{
+    public static void Run()
+    {
+        // Write a program to input the month number and print the number of days in that month.
+        // Example
+        // Input: Month Number: 1
+        // Output: Days in Month: 31
+        int month = int.Parse(Console.ReadLine());
+        DateTime date = new DateTime(2021, month, 1);
+        Console.WriteLine(date.AddMonths(1).AddDays(-1).Day);
+        
+    }
+}
+
+internal static class P10
+{
+    public static void Run()
+    {
+        /*
+         * 17- Create a program that asks the user to input three points
+         * (x1, y1), (x2, y2), and (x3, y3), and determines whether these
+         * points lie on a single straight line.
+         */
+        Console.WriteLine("Enter the first point (x1, y1):");
+        string[] point1 = Console.ReadLine().Split(' ');
+        Console.WriteLine("Enter the second point (x2, y2):");
+        string[] point2 = Console.ReadLine().Split(' ');
+        Console.WriteLine("Enter the third point (x3, y3):");
+        string[] point3 = Console.ReadLine().Split(' ');
+        
+        double x1 = double.Parse(point1[0]);
+        double y1 = double.Parse(point1[1]);
+        double x2 = double.Parse(point2[0]);
+        double y2 = double.Parse(point2[1]);
+        double x3 = double.Parse(point3[0]);
+        double y3 = double.Parse(point3[1]);
+        
+        double slope1 = (y2 - y1) / (x2 - x1);
+        double slope2 = (y3 - y2) / (x3 - x2);
+        
+        if (slope1 == slope2)
+        {
+            Console.WriteLine("The points lie on a single straight line.");
+        }
+        else
+        {
+            Console.WriteLine("The points do not lie on a single straight line.");
+        }
+
+    }
+}
+
+internal static class P11
+{
+    public static void Run()
+    {
+        /*
+         * Within a company, the efficiency of workers is evaluated based on the duration
+         * required to complete a specific task. A worker's efficiency level is determined
+         * as follows: 
+            - If the worker completes the job within 2 to 3 hours, they are considered
+              highly efficient. 
+            - If the worker takes 3 to 4 hours, they are instructed to increase their speed. 
+            - If the worker takes 4 to 5 hours, they are provided with training to enhance
+              their speed. 
+            - If the worker takes more than 5 hours, they are required to leave the company. 
+            To calculate the efficiency of a worker, the time taken for the task is obtained
+            via user input from the keyboard.
+         */
+        Console.WriteLine("Enter the time taken to complete the task:");
+        double time = double.Parse(Console.ReadLine());
+        
+        if (time >= 2 && time < 3)
+        {
+            Console.WriteLine("The worker is highly efficient.");
+        }
+        else if (time >= 3 && time < 4)
+        {
+            Console.WriteLine("The worker is instructed to increase their speed.");
+        }
+        else if (time >= 4 && time < 5)
+        {
+            Console.WriteLine("The worker is provided with training to enhance their speed.");
+        }
+        else if (time >= 5)
+        {
+            Console.WriteLine("The worker is required to leave the company.");
+        }
+
+    }
+}
+
+internal static class P12
+{
+    public static void Run()
+    {
+        /*
+         * Write C# program that Assigning one value type variable to another and
+         * modifying the value of one variable and mention what will happen
+         */
+        int a = 5;
+        int b = a;
+        b = 10;
+        Console.WriteLine(a); // 5
+        Console.WriteLine(b); // 10
+        // The value of a will not change because it is a value type variable.
+    }
+}
+
+internal static class P13
+{
+    public static void Run()
+    {
+        /*
+         * Write C# program that Assigning one reference type variable to another and
+         * modifying the object through one variable and mention what will happen
+         */
+        int[] a = { 1, 2, 3 };
+        int[] b = a;
+        b[0] = 10;
+        Console.WriteLine(a[0]); // 10
+        Console.WriteLine(b[0]); // 10
+        // The value of a will change because it is a reference type variable.
+
+    }
+}
+
+internal static class P14
+{
+    public static void Run()
+    {
+        /*
+         * Which of the following statements is correct about the C#.NET code snippet
+         * given below?
+         * 
+         * int d; 
+         * d = Convert.ToInt32( !(30 < 20) );
+         * 
+         * a) A value 0 will be assigned to d.
+         * b) A value 1 will be assigned to d.
+         * c) A value -1 will be assigned to d.
+         * d) The code reports an error.
+         * e) The code snippet will work correctly if ! is replaced by Not.
+         */
+        int d;
+        d = Convert.ToInt32( !(30 < 20) );
+        Console.WriteLine(d); // 1
+        // b) A value 1 will be assigned to d.
+        // as true = 1 and false = 0 and condition is false so !false = true = 1
+    }
+}
+
+internal static class P15
+{
+    public static void Run()
+    {
+        /*
+         * Which of the following is the correct output for the C# code given below?
+         * 
+         * Console.WriteLine(13 / 2 + " " + 13 % 2); 
+         * 
+         * a) 6.5 1
+         * b) 6.5 0
+         * c) 6 0
+         * d) 6 1
+         * e) 6.5 6.5
+         */
+        Console.WriteLine(13 / 2 + " " + 13 % 2); // 6 1
+        // c) 6 1
+        // 13 / 2 = 6 and 13 % 2 = 1 so when we concatenate them it will be 6 1
+
+    }
+}
+
+internal static class P16
+{
+    public static void Run()
+    {
+        /*
+         * What will be the output of the C# code given below?
+         * 
+         * int num = 1, z = 5;
+         * 
+         * if (!(num <= 0))
+         *     Console.WriteLine( ++num + z++ + " " + ++z ); 
+         * else
+         *     Console.WriteLine( --num + z-- + " " + --z ); 
+         * 
+         * a) 5 6
+         * b) 6 5
+         * c) 6 6
+         * d) 7 7
+         */
+        int num = 1, z = 5;
+
+        if (!(num <= 0))
+            Console.WriteLine( ++num + z++ + " " + ++z ); 
+        else
+            Console.WriteLine( --num + z-- + " " + --z ); 
+        
+        // b) 7 7
+        // first condition !(false) = true so it will print 2 + 5 + " " + 7 = 7 7
+
+    }
+}
---
+++ DemoNotes.cs
@@ -0,0 +1,454 @@
+﻿using System.Collections;
+
+namespace Session_03;
+
+public static class DemoNotes
+{
+
+    public static void Run()
+    {
+        #region Value Type Casting
+
+        #region Implicit Casting — Safe Casting
+
+        // int x = 4; // 4 Bytes
+        // long Y = /*(long)*/x; // 8 Bytes
+        // // here casting from int to long is done implicitly because long is bigger than int
+
+        #endregion
+
+        #region Explict Casting — UnSafe Castihg
+
+        // long X = 101010101010; // 8 Bytes
+        // int Y = (int)X;
+        // Console.WriteLine(Y);
+        // // here casting from long to int is done explicitly because int is smaller than long
+        // // if the value of X is bigger than the max value of int, there will be an exception overflow
+
+        #endregion
+
+        #endregion
+
+        #region Parse [Convert from String to any Datatype]
+
+        // int Number = int.Parse(Console.ReadLine());
+        // Console. WriteLine("Enter Data User");
+        // Console.WriteLine("Enter Name: ");
+        // string Name = Console.ReadLine();
+        // Console.WriteLine("Enter Age: ");
+        // int Age = Int32.Parse(Console.ReadLine());
+        // // int = Int32
+        // Console.WriteLine("Enter Salary: ");
+        // double Salary = double.Parse(Console.ReadLine());
+        // // each datatype has a parse method to convert from string to the datatype
+
+        #endregion
+
+        #region Convert [Convert from Any Datatype to Any DateType]
+
+        // int Number = Convert.ToInt32(Console.ReadLine());
+        // Console. WriteLine("Enter Data User");
+        // Console.WriteLine("Enter Name: ");
+        // string Name = Console.ReadLine();
+        // Console.WriteLine("Enter Age: ");
+        // int Age = Convert.ToInt32(Console.ReadLine());
+        // // int = Int32
+        // Console.WriteLine("Enter Salary: ");
+        // double Salary = Convert.ToDouble(Console.ReadLine());
+        // // each datatype has a parse method to convert from string to the datatype
+
+        #endregion
+
+        #region Opreators
+
+        #region Unary Operators [++, --]
+
+        // int X = 5;
+        // X++; // X = X + 1;
+        // --X; // X = X - 1;
+        // Console.WriteLine(++X); // 5 //print then add 1
+        // Console.WriteLine(X++); // 7 //add 1 then print 
+
+        #endregion
+
+        #region Binary Operators [+, -, *, /, %]
+        // int Sum, Mul, Sub, Div, Mod;
+        // int X = 5, Y = 3;
+        // Sum = X + Y;// 8
+        // Mul = X * Y;// 15
+        // Sub = X - Y;// 2
+        // Div = X / Y;// 1
+        // Mod = X % Y;// 2
+        // // mod is the remainder of the division
+
+        #endregion
+
+        #region Assignment Operator
+
+        //
+        // int X = 10;
+        // X += 10; // x = x + 10
+        // X -= 10; 
+        // X *= 10;
+        // X /= 10;
+        // X %= 10;
+
+        #endregion
+
+        #region Comparison Operator
+
+        // int X = 5, Y = 3;
+        // bool IsEqual = X == Y; // false
+        // bool IsNotEqual = X != Y; // true
+        // bool IsGreater = X > Y; // true
+        // bool IsLess = X < Y; // false
+        // bool IsGreaterOrEqual = X >= Y; // true
+
+        #endregion
+
+        #region Logical Operator [Short Circuit]
+
+        // bool IsTrue = true, IsFalse = false;
+        // // AND
+        // // true && true = true
+        // // true && false = false
+        // // false && true = false
+        // // false && false = false
+        //
+        // // OR
+        // // true || true = true
+        // // true || false = true
+        // // false || true = true
+        // // false || false = false
+
+        #endregion
+
+        #region Bitwise Operator [Long Circuit]
+
+        // bool IsTrue = true, IsFalse = false;
+        // // AND
+        // // true  & true = true
+        // // true  & false = false
+        // // false &  true = false
+        // // false &  false = false
+        //
+        // // OR
+        // // true  | true = true
+        // // true  | false = true
+        // // false |  true = true
+        // // false |  false = false
+
+        #endregion
+
+        #region Ternary Operator [Condition Operator]
+
+        // int X = 4;
+        // string Message = X > 4 ? "X Greater Than 4" : "X Less than Or Equal 4";
+        // Console.WriteLine(Message);
+        // // nested ternary operator
+        // Message = X > 4 ? "X Greater Than 4" : X < 4 ? "X Less than 4" : "X Equal 4";
+
+        #endregion
+        #endregion
+
+        #region Operator Periorety
+
+        // /*
+        // 1. Unary Operator [PreFix]
+        // 2. ()
+        // 3. * / %
+        // 4. + -
+        // */
+        // int A = 20;
+        // int B = 15;
+        // int C = 10;
+        // int D = 5;
+        // int Result; 
+        // Result = (A + B) * C / D;// 70
+        // Result = A++ * C; // 200
+        // Console.WriteLine(Result);
+
+        #endregion
+
+        #region String Formating
+
+        // // Equation = X + Y = Result
+        // // Equation = 10 + 5 = 15
+        // int X = 5;
+        // int Y = 10;
+        // int Result = X + Y;
+
+        #region 0. Normal Way
+
+        // Console.WriteLine("Equation: "+ X + " + " + Y+" = "+Result);
+
+        #endregion
+
+        #region 1. Composite Format
+
+        // String.Format("Equation = {0} + {1} = {2}", X, Y, Result);
+
+        #endregion
+
+        #region 2.String Manipolition
+
+        // // string Interpolation => $
+        // Console.WriteLine($"Equation = {X} + {Y} = {Result}");
+
+        #endregion
+
+        #endregion
+
+        #region IF Else - Switch Case
+
+        // Console.WriteLine("Enter Month Number");
+        //  int MonthNumber = int.Parse(Console.ReadLine());
+        // // 1 => Month is Jan
+        // // 2 => Month is Feb
+        // // 3 => Month is Mar
+        // // Invalid Input
+
+        #region IF Else
+
+        // if (MonthNumber == 1)
+        // {
+        //     Console.WriteLine("Month is Jan");
+        // }
+        // else if (MonthNumber == 2)
+        // {
+        //     Console.WriteLine("Month is Feb");
+        // }
+        // else if (MonthNumber == 3)
+        // {
+        //     Console.WriteLine("Month is Mar");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Invalid Input");
+        // }
+
+        #endregion
+
+        #region Switch Case
+
+        // switch (MonthNumber)
+        // {
+        //     case 1:
+        //         Console.WriteLine("Month is Jan");
+        //         break;
+        //     case 2:
+        //         Console.WriteLine("Month is Feb");
+        //         break;
+        //     case 3:
+        //         Console.WriteLine("Month is Mar");
+        //         break;
+        //     default:
+        //         Console.WriteLine("Invalid Input");
+        //         break;
+        // }
+
+        #endregion
+
+        // switch case is faster than if else
+        // as switch case is implemented using jump table
+        // if else is implemented using if else ladder
+
+        #region Switch Case with String
+
+        // Console.WriteLine("Enter Name");
+        // string Name = Console.ReadLine();
+        // switch (Name)
+        // {
+        //     case "Mostafa":
+        //     case "mostafa": // it like OR (Mostafa || mostafa)
+        //         Console.WriteLine("Hello Mostafa");
+        //         break;
+        //     case "Aliaa":
+        //         Console.WriteLine("Hello Aliaa");
+        //         break;
+        //     case "Ahmed":
+        //         Console.WriteLine("Hello Ahmed");
+        //         break;
+        //     default:
+        //         Console.WriteLine("Hello Stranger");
+        //         break;
+        // }
+        // // switch case with string not created using jump table if it is smaller than 5 cases
+        // // if it is bigger than 5 cases, it will be created using jump table
+        // // in numeric switch case, jump table is created with no conditions
+
+        #endregion
+
+        #region GoTo
+
+        // // GoTo is a keyword to jump to a specific label
+        // goto here;
+        // Console.WriteLine("false");
+        // here:
+        // Console.WriteLine("true");
+        // Console.WriteLine("Enter Option");
+        // // 3000 => "Option 01", "Option 02". "Option 03"
+        // // 2000 => "Option 01", "Option 02"
+        // // 1000 => "Option 01"
+        //
+        //
+        // int Option = int.Parse(Console.ReadLine());
+        // switch (Option)
+        // {
+        //     case 3000:
+        //         Console.WriteLine("Option 03");
+        //         goto case 2000;
+        //     case 2000:
+        //         Console.WriteLine("Option 02");
+        //         goto case 1000;
+        //     case 1000:
+        //         Console.WriteLine("Option 01");
+        //         break;
+        // }
+
+        #endregion
+
+        #region Evaluation Of Switch Case
+
+        // https://mihirdave95.medium.com/evaluation-of-switch-in-c-be587f7de47c
+
+        #region C# 6.0
+
+        // // in C# 6.0, switch case can evaluate the condition
+        // Console.WriteLine("Enter Age");
+        // int Age = int.Parse(Console.ReadLine());
+        // switch(Age)
+        // {
+        //     case > 22:
+        //         Console.WriteLine("Age Greater Than 22");
+        //         break;
+        //     case < 22:
+        //         Console.WriteLine("Age Less Than 22");
+        //         break;
+        //     default:
+        //         Console.WriteLine("Age Equal 22");
+        //         break;
+        // }
+
+        #endregion
+
+        #region C# 7.0
+
+        // object obj = new object();
+        // obj = "Mostafa";
+        // obj = 10;
+        // obj = true;
+        // switch(obj)
+        // {
+        //     case int Number when Number > 10 && Number < 20:
+        //         Console.WriteLine($"{Number} is int");
+        //         break;
+        //     case string Name:
+        //         Console.WriteLine($"{Name} is String");
+        //         break;
+        //     case bool IsTrue:
+        //         Console.WriteLine($"{IsTrue} is Boolean");
+        //         break;
+        //     default:
+        //         Console.WriteLine("Unknown Type");
+        //         break;
+        // }
+
+        #endregion
+
+        #region C# 8.0
+
+        #region EXample 01
+
+        // Console.WriteLine("Enter Option");
+        // int Opation = int.Parse(Console.ReadLine());
+        // // Switch Expression
+        // string Message = Opation switch
+        // {
+        //     1 => "Option 01",
+        //     2 => "Option 02",
+        //     3 => "Option 03",
+        //     _ => "Invalid"
+        // };
+        // Console.WriteLine(Message);
+
+        #endregion
+
+        #region Example 02
+        // Empolyee E01 = new Empolyee();
+        // E01.Name = "Ali";
+        // E01.Id = 10;
+        // E01. Age = 22;
+        // string Massege = E01 switch
+        // {
+        //     { Id:10, Name: "Alaa", Age: 22}=>$"{E01.Id} {E01.Name} {E01.Age}",
+        //     { Id:10, Name: "Ali", Age: 22}=> $"Do Something",
+        //     { Id:20, Name: "Mona", Age: 20}=> $"{E01.Id} : {E01.Name} {E01.Age}"
+        // };
+
+        #endregion
+        #endregion
+
+        #region C# 9.0 
+        // // tha add Relational patterns and Logical patterns
+        // Empolyee E01 = new Empolyee();
+        // E01.Name = "Ali";
+        // E01.Id = 10;
+        // E01. Age = 22;
+        // string Massege = E01 switch
+        // {
+        //     {  Age: > 22 and < 26}=>$"{E01.Id} {E01.Name} {E01.Age}",
+        //     { Id:10, Name: "Ali", Age: 22 or 30}=> $"Do Something",
+        //     { Id:20, Name: "Mona", Age: 20}=> $"{E01.Id} : {E01.Name} {E01.Age}"
+        // };
+        #endregion
+        #endregion
+
+
+
+        #endregion
+        
+        #region Notes
+        // // any implicit casting is a safe casting
+        // // string is emutable
+        // // _ is used to ignore the value it call Discard
+        //
+        //
+        // // Switch case is faster than if else as it use jump table
+        // // if else is slower than switch case as it use if else ladder
+        // // Switch case is working with string in C#
+        // // Box and UnBox
+        // // Box => Convert Value Type to Reference Type
+        // // UnBox => Convert Reference Type to Value Type
+        // // Boxing is slower than UnBoxing
+        // //Ex 01:
+        // int X = 10;
+        // object obj = X; // Boxing
+        // int Y = (int)obj; // UnBoxing
+        //
+        // //Ex 02:
+        // string Chicken = "chicken";
+        // int Fish = 1;
+        // ArrayList Truck = new ArrayList();
+        // Truck.Add(Chicken); // Boxing  //it will convert the string to object
+        // Truck.Add(Fish);    // Boxing
+        // Truck[1] = (int)Truck[1] + 1; // UnBoxing //it will convert the object to int 
+        //
+        // // Boxing is slower than UnBoxing
+        
+        
+        
+        #endregion
+        
+    }
+
+}
+internal class Empolyee
+{
+
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+}
